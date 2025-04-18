5232e39023d15074a57ccfa51cff854595455919
Glen Wamaru <garpirungu@gmail.com>
2025-04-18 08:51:31 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+  function superbowlWin(record) {
+    function isWin(game) {
+        return game.result === "W";
+    }
+
+    var win = record.find(isWin);
+
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
+console.log(superbowlWin(record)); 
